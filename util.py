7be3912bd0fd94d6e921f99958a0f113f695f7bb
John Nagel <jnagel@iastate.edu>
2018-01-03 17:26:04 -0600

    Added B-Scan Slicing Function to util

--- util.py
+++ util.py
@@ -14,3 +14,29 @@ def guassian_curve(x, a, b, c):
     y = a * np.exp(-(x-b)**2 / (2*c**2))
 
     return y
+
+
+def b_scan_slicer(data, save_loc):
+    """
+    Creates an image of each B-Scan slice and saves them to the directory specified by save_loc.
+    The direction of the B-Scan is specified by the data.b_scan_dir; either horizontal or vertical.
+    data: An instance of a THzData class
+    save_loc: string that specifies the directory to save the images to
+    """
+
+    if data.b_scan_dir == 'horizontal':
+        b_scan_layers = np.zeros((data.y_step, data.wave_length, data.x_step))
+        for i in range(data.y_step):
+            data.make_b_scan(i, 0)
+            b_scan_layers[i] = data.b_scan
+
+    elif data.b_scan_dir == 'vertical':
+        b_scan_layers = np.zeros((data.x_step, data.wave_length, data.y_step))
+        for i in range(data.x_step):
+            data.make_b_scan(0, i)
+            b_scan_layers[i] = data.b_scan
+
+    else:
+        raise ValueError("B-Scan direction must be either 'horizontal' or 'vertical'")
+
+    return b_scan_layers
