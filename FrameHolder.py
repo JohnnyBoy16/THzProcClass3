72cbbec50570614e9ffda18aabf2698ebfc33b52
John Nagel <jnagel@iastate.edu>
2018-02-01 17:10:24 -0600

    Added Frequency domain option to A-Scan Frame

--- FrameHolder.py
+++ FrameHolder.py
@@ -10,9 +10,9 @@ class FrameHolder:
     """
     # this class allows each frame to cause actions in another by linking them
     # through a holder class
-    def __init__(self, data):
+    def __init__(self, data, a_scan_only=True):
 
-        self.a_scan_frame = AScanFrame(self, data)
+        self.a_scan_frame = AScanFrame(self, data, a_scan_only)
         self.raw_c_scan_frame = RawCScanFrame(self, data)
         self.b_scan_frame = BScanFrame(self, data)
         self.interpolated_c_scan_frame = InterpolatedCScanFrame(self, data)
