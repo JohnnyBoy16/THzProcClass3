4d0a0f84ac57bf2ede1ae20930b2bf4a3dbf6eee
John Nagel <ironmanwatch@hotmail.com>
2018-06-15 13:03:44 -0500

    Added docstring to descript input params

--- FrameHolder.py
+++ FrameHolder.py
@@ -12,6 +12,15 @@ class FrameHolder:
     # through a holder class
 
     def __init__(self, data, a_scan_only=True):
+        """
+        Constructor method. Sets all of the instance attributes and allows
+        the frames to be linked together
+        :param data: An instance of the THzData class
+        :param a_scan_only: Either True or False depending on whether or not
+            the user wants to see the frequency domain information
+            (Default: False)
+        """
+
         self.a_scan_frame = AScanFrame(self, data, a_scan_only)
         self.raw_c_scan_frame = RawCScanFrame(self, data)
         self.b_scan_frame = BScanFrame(self, data)
