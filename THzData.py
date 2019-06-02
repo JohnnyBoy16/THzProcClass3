34fe4984b4aa0c714611693030c1252c828dea38
John Nagel <ironmanwatch@hotmail.com>
2019-06-02 14:38:38 -0500

    Corrected calculation of B-Scan extent values
    Corrected calculation of B-Scan extent values for plotting in
    make_b_scan() function. Added extra half pixel width to all sides to
    account for matplotlib indexing pixels in the center of the pixel.

--- THzData.py
+++ THzData.py
@@ -623,10 +623,15 @@ class THzData(object):
         # transpose after arranging the data
         if self.b_scan_dir == 'horizontal':
             self.b_scan = self.waveform[yid, :, :]
-            self.b_scan_extent = (self.x[0], self.x[-1], 0, self.time[-1])
+            self.b_scan_extent = (self.x[0] - self.true_x_res/2,
+                                  self.x[-1] + self.true_x_res/2,
+                                  0 - self.dt/2, self.time[-1] + self.dt/2)
+
         else:  # b_scan_dir == 'vertical'
             self.b_scan = self.waveform[:, xid, :]
-            self.b_scan_extent = (self.y[0], self.y[-1], 0, self.time[-1])
+            self.b_scan_extent = (self.y[0] - self.true_y_res/2,
+                                  self.y[-1] + self.true_y_res/2,
+                                  0 - self.dt/2, self.time[-1] + self.dt/2)
 
         # call to transpose is necessary to flip data axis, so x or y location
         # is on the bottom and time is along the y-axis on the plot
